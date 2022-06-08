3238f1b9d9851283ba560fa1fe34b3499e661fc6
ChinaBUG <seaone@qq.com>
2022-06-08 17:02:42 +0800

    Create how-2-append-js-2-body.js

---
+++ how-2-append-js-2-body.js
@@ -0,0 +1,20 @@
+/*
+ * @Author: ChinaBUG
+ * @Date: 2022-06-08 16:59:09
+ * @LastEditTime: 2022-06-08 17:00:19
+ * @LastEditors: pptogo-110
+ * @Description: 将脚本附加到页面的body内
+ * @FilePath: \js-utils\how-2-append-js-2-body.js
+ */
+
+/*
+javascript:(function(){var w=window,d=w.document,b=d.body,s=d.createElement("script");s.src="https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download.js";b.appendChild(s);})();
+*/
+javascript: (function() {
+    var w = window,
+        d = w.document,
+        b = d.body,
+        s = d.createElement("script");
+    s.src = "https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download.js";
+    b.appendChild(s);
+})();
