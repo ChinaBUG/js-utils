4b827789e00e5aeb2072d8933fd0ec6e08cc3e03
ChinaBUG <seaone@qq.com>
2022-06-19 09:13:12 +0800

    Update how-2-append-js-2-body.js

--- how-2-append-js-2-body.js
+++ how-2-append-js-2-body.js
@@ -8,7 +8,7 @@
  */
 
 /*
-javascript:(function(){var w=window,d=w.document,b=d.body,s=d.createElement("script");s.src="https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v2.js";b.appendChild(s);})();
+javascript:(function(){var de=new Date(),version=de.getYear()+'.'+(de.getMonth()+1)+'.'+de.getDate()+'.'+de.getHours()+'.'+de.getMinutes();var w=window,d=w.document,b=d.body,s=d.createElement("script");s.src="https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v2.js#"+version;b.appendChild(s);})();
 */
 javascript: (function() {
     var de=new Date(),version=de.getYear()+'.'+(de.getMonth()+1)+'.'+de.getDate()+'.'+de.getHours()+'.'+de.getMinutes();
