bfe08ade8f963ec85aa8d7a15ea24c11d354d639
ChinaBUG <seaone@qq.com>
2022-06-19 10:25:30 +0800

    Update how-2-append-js-2-body.js

--- how-2-append-js-2-body.js
+++ how-2-append-js-2-body.js
@@ -8,7 +8,7 @@
  */
 
 /*
-javascript:(function(){var de=new Date(),version=de.getYear()+'.'+(de.getMonth()+1)+'.'+de.getDate()+'.'+de.getHours()+'.'+de.getMinutes();var w=window,d=w.document,b=d.body,s=d.createElement("script");s.src="https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v2.js#"+version;b.appendChild(s);})();
+javascript:(function(){var de=new Date(),version=de.getYear()+'.'+(de.getMonth()+1)+'.'+de.getDate()+'.'+de.getHours()+'.'+de.getMinutes();var w=window,d=w.document,b=d.body,s=d.createElement("script");s.src="https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v3.js#"+version;b.appendChild(s);})();
 */
 javascript: (function() {
     var de=new Date(),version=de.getYear()+'.'+(de.getMonth()+1)+'.'+de.getDate()+'.'+de.getHours()+'.'+de.getMinutes();
@@ -16,6 +16,6 @@ javascript: (function() {
         d = w.document,
         b = d.body,
         s = d.createElement("script");
-    s.src = "https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v2.js#"+version;
+    s.src = "https://chinabug.github.io/js-utils/case/case-youdianshop-com-batch-download-v3.js#"+version;
     b.appendChild(s);
 })();
